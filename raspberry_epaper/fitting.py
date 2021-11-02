418570f7c30026953641e4aa02002a52d520f7a2
yskoht <ysk.oht@gmail.com>
2021-11-03 00:44:56 +0900

    Fix fitting

--- raspberry_epaper/fitting.py
+++ raspberry_epaper/fitting.py
@@ -1,6 +1,29 @@
 import logging
 
-def fitting(foreImage, backImage):
+def crop(foreImage, backImage):
+    fw, fh = foreImage.size
+    bw, bh = backImage.size
+
+    fa = float(fw) / float(fh)
+    ba = float(bw) / float(bh)
+
+    logging.info('{:.3f}, {:.3f}'.format(fa, ba))
+
+    # 4:3 ~ 16:9
+    if not 1.332 < fa < 1.778:
+        return foreImage
+
+    if fa < ba:
+        nh = fw / ba
+        logging.info('nh: {:.3f}'.format(nh))
+        return foreImage.crop((0, (fh - nh) // 2, fw, (fh + nh) // 2))
+    else:
+        nw = ba * fh
+        logging.info('nw: {:.3f}'.format(nw))
+        return foreImage.crop(((fw - nw) // 2, 0, (fw + nw) // 2, fh))
+
+
+def resize(foreImage, backImage):
     fw, fh = foreImage.size
     bw, bh = backImage.size
 
@@ -11,13 +34,21 @@ def fitting(foreImage, backImage):
 
     if fa < ba:
         nw = int(float(bh) / float(fh) * fw)
-        img = foreImage.resize((nw, bh))
+        return foreImage.resize((nw, bh))
     else:
         nh = int(float(bw) / float(fw) * fh)
-        img = foreImage.resize((bw, nh))
+        return foreImage.resize((bw, nh))
 
-    w, h = img.size
+
+def fitting(_foreImage, backImage):
+    cropped = crop(_foreImage, backImage)
+    resized = resize(cropped, backImage)
+
+    w, h = resized.size
+    bw, bh = backImage.size
+
     x = int((bw - w) / 2.0)
     y = int((bh - h) / 2.0)
-    backImage.paste(img, (x, y))
+    backImage.paste(resized, (x, y))
+
     return backImage
